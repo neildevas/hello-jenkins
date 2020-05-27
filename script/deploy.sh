b8b366ee70b8fc3e4351c6bc0ce2ee3939515882
Neil Devas <neil@keyo.com>
2020-05-27 17:45:04 -0400

    update deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,8 @@ ssh devas@161.35.134.233 <<EOF
  cd ~/hello-jenkins
  ls
 echo 'pulling files now'
+git fetch origin
+git reset --hard origin/master
  git pull
  npm install — production
  pm2 restart all
