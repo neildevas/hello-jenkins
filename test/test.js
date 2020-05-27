759210ae163d71105403417ac62fe63f520516d8
Neil Devas <neil@keyo.com>
2020-05-27 17:36:19 -0400

    fix test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ const app = require('../index.js');
 describe('GET /', function() {
   it('respond with hello world', function(done) {
     //navigate to root and check the the response is "hello world"
-    request(app).get('/').expect('hello world', done);
+    request(app).get('/').expect('hey world I was deployed from Jenkins!!!', done);
   });
 });
