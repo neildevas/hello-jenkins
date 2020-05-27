cf000aefb1896d9f1dc3ea043bf3c24f9138df4b
Neil Devas <neil@keyo.com>
2020-05-27 14:33:15 -0400

    simple node app with test

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../index.js');
+
+describe('GET /', function() {
+  it('respond with hello world', function(done) {
+    //navigate to root and check the the response is "hello world"
+    request(app).get('/').expect('hello world', done);
+  });
+});
